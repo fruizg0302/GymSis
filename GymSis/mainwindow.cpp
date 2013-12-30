60fe589859ef5e818ba701ff8571da3874fd3508
fruizg0302 <fruizg0302@klinkmobile.com>
2013-12-30 12:56:06 -0600

    more changes
    and stuff

--- GymSis/mainwindow.cpp
+++ GymSis/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QLabel.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLabel* label1 = new QLabel("One");
 }
 
 MainWindow::~MainWindow()
